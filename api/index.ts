5f24063b5cc64c2324d0b187cc6143909a496cda
Luhanvinicius <luhandev.vini@gmail.com>
2025-11-14 16:12:12 -0300

    Usar require ao invÃ©s de import no api/index (JavaScript compilado)

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import serverless from "serverless-http";
-// Importar direto do TypeScript - Vercel compila automaticamente
-import app from "../src/app";
+// Importar do código compilado (JavaScript)
+const app = require("../dist/app");
 
-const handler = serverless(app as any);
+const handler = serverless(app.default || app);
 
 export default async function (req: any, res: any) {
   return handler(req, res);
